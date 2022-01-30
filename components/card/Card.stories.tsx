322da8e74a0a241e9ee1048c1a7dfe921ce51408
Blake Bradford <bbradforddesign@gmail.com>
2022-01-30 17:26:20 -0600

    convert first story to tailwind

--- components/card/Card.stories.tsx
+++ components/card/Card.stories.tsx
@@ -8,211 +8,87 @@ export default {
 } as ComponentMeta<typeof Card>;
 
 export const Active: ComponentStory<typeof Card> = () => (
-    <Card width="50vw">
-        <div className="header">
-            <h2>Medium</h2>
-            <button className="toggle">&mdash;</button>
-        </div>
+    <div className="w-1/2">
+        <Card>
+            <div className="w-full flex justify-between items-center">
+                <h2 className="text-xl font-bold">Medium</h2>
+                <button>&mdash;</button>
+            </div>
 
-        <p className="date">Remote, 2000-2010</p>
+            <p className="text-md italic">Remote, 2000-2010</p>
 
-        <div className="body">
-            <ul id="details">
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-                <li>
-                    Lorem Ipsum dolor sit amet, consectetur adipiscing elit, set
-                </li>
-            </ul>
-            <div id="icons">
-                <div>
-                    <h3>Tools</h3>
-                    <ul className="icon-grid">
-                        <li>
-                            <Icon name="html" color="orange" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="css" color="blue" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="react" color="cyan" size="sm" />
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Tools</h3>
-                    <ul className="icon-grid">
-                        <li>
-                            <Icon name="html" color="orange" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="css" color="blue" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="react" color="cyan" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="go" color="cyan" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="html" color="orange" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="css" color="blue" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="react" color="cyan" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="go" color="cyan" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="html" color="orange" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="css" color="blue" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="react" color="cyan" size="sm" />
-                        </li>
-                        <li>
-                            <Icon name="go" color="cyan" size="sm" />
-                        </li>
-                    </ul>
+            <div className="text-sm md:flex md:flex-col lg:flex-row lg:justify-between">
+                <ul className="mt-4 lg:flex-1 lg:max-w-2xl lg:mr-4">
+                    <li>
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit,
+                        set
+                    </li>
+                    <li>
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit,
+                        set
+                    </li>
+                    <li>
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit,
+                        set
+                    </li>
+                </ul>
+                <div className="mt-4 md:w-full md:flex md:flex-row md:justify-between lg:w-1/4 lg:flex-col lg:max-w-xs lg:justify-start">
+                    <div className="md:w-2/5 lg:w-full">
+                        <h3>Tools</h3>
+                        <ul className="list-none flex flex-row flex-wrap p-0 lg:w-full">
+                            <li className="m-1">
+                                <Icon name="html" color="orange" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="css" color="blue" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="react" color="cyan" size="sm" />
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="md:w-2/5 lg:w-full">
+                        <h3>Tools</h3>
+                        <ul className="list-none flex flex-row flex-wrap p-0 lg:w-full">
+                            <li className="m-1">
+                                <Icon name="html" color="orange" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="css" color="blue" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="react" color="cyan" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="go" color="cyan" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="react" color="cyan" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="go" color="cyan" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="react" color="cyan" size="sm" />
+                            </li>
+                            <li className="m-1">
+                                <Icon name="go" color="cyan" size="sm" />
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-        <style jsx>{`
-            * {
-                font-family: "Roboto", sans-serif;
-            }
-            .header {
-                width: 100%;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-            }
-            .toggle {
-                padding: 0;
-                border: none;
-                background: none;
-                font-weight: bold;
-                font-size: 1.5rem;
-            }
-            .date {
-                font-style: italic;
-            }
-            .icon-grid {
-                display: flex;
-                flex-direction: row;
-                flex-wrap: wrap;
-                padding: 0;
-            }
-            .icon-grid li {
-                margin: 0.5rem;
-                list-style: none;
-            }
-
-            @media (min-width: 834px) {
-                .body {
-                    display: flex;
-                    flex-direction: column;
+            <style jsx>{`
+                * {
+                    font-family: "Roboto", sans-serif;
                 }
-
-                #icons {
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: space-between;
-                }
-
-                #icons div {
-                    width: 45%;
-                }
-            }
-
-            @media (min-width: 1440px) {
-                .body {
-                    flex-direction: row;
-                    justify-content: space-between;
-                }
-
-                #details {
-                    flex: 1;
-                    max-width: 50rem;
-                    margin-right: 2rem;
-                }
-
-                #icons {
-                    flex-direction: column;
-                    max-width: 12rem;
-                    justify-content: flex-start;
-                }
-
-                #icons div {
-                    width: 100%;
-                }
-            }
-        `}</style>
-    </Card>
+            `}</style>
+        </Card>
+    </div>
 );
 
 export const Inactive: ComponentStory<typeof Card> = () => (
-    <Card width="450px">
+    <Card>
         <div className="header">
             <h2>Medium</h2>
             <button className="toggle">&mdash;</button>
--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,21 +1,11 @@
 type Props = {
-    width: string;
     children: React.ReactNode;
 };
 
-export const Card: React.FC<Props> = ({ children, width }) => {
+export const Card: React.FC<Props> = ({ children }) => {
     return (
-        <section className="card">
+        <div className="w-full p-4 rounded-xl shadow-md shadow-slate-400">
             {children}
-            <style jsx>{`
-                .card {
-                    width: ${width};
-                    padding: 1rem 2rem;
-                    background-color: #fff;
-                    border-radius: 1rem;
-                    box-shadow: 0.35rem 0.35rem 0.5rem rgba(196, 196, 196, 0.25);
-                }
-            `}</style>
-        </section>
+        </div>
     );
 };
