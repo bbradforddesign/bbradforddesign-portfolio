2aff4ae4ddc7bf68d36c7d1ee8c862e6f53083c1
Blake Bradford <bbradforddesign@gmail.com>
2022-02-01 13:28:04 -0600

    About section layout, remove unused flex-grow

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,11 +2,13 @@ export const About: React.FC = () => {
     return (
         <section className="section-container">
             <h2 className="font-bold text-3xl self-start m-8">About</h2>
-            <div className="lg:flex lg:flex-row mx-12">
-                <div className="w-1/2 h-72 bg-slate-100" />
-                <div className="w-1/2">
-                    <p>Nice to Meet You!</p>
-                    <p>
+            <div className="md:flex md:flex-row mx-12">
+                <div className="w-full md:w-1/2 h-72 bg-slate-100" />
+                <div className="w-full my-4 md:w-1/2 md:my-0 md:ml-4">
+                    <p className="font-bold text-xl my-4 md:mt-0">
+                        Nice to Meet You!
+                    </p>
+                    <p className="body-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -17,7 +19,7 @@ export const About: React.FC = () => {
                         non proident, sunt in culpa qui officia deserunt mollit
                         anim id est laborum.
                     </p>
-                    <p>
+                    <p className="body-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua. Ut enim ad minim veniam, quis nostrud
