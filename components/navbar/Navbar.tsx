9a8dca04d7559d985f14f1a4f99ea19afe2deccb
Blake Bradford <bbradforddesign@gmail.com>
2022-02-13 00:06:06 -0600

    first draft dropdown animation

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -18,8 +18,8 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
     ));
 
     return (
-        <nav className="flex flex-col justify-between items-start p-4 w-full sticky top-0 -mb-12 z-10 opacity-95 backdrop-blur-lg bg-white dark:bg-slate-900 border-b-2 lg:flex-row lg:items-center">
-            <div className="flex flex-row justify-between items-center w-full">
+        <nav className="flex flex-col justify-between items-start p-4 w-full sticky top-0 -mb-12 z-30 opacity-95 backdrop-blur-lg bg-white dark:bg-slate-900 lg:flex-row lg:items-center">
+            <div className="flex flex-row justify-between items-center w-full z-30">
                 <Link href="#">
                     <a>
                         <h1 className="font-bold text-xl">bbradforddesign</h1>
@@ -38,9 +38,9 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
                 </button>
             </div>
             <ul
-                className={`${openNav || "hidden"} ${
-                    !openNav || "flex"
-                } flex-col w-full absolute top-14 left-0 pb-4 lg:pb-0 bg-white dark:bg-slate-900 border-b-2 lg:relative lg:top-0 lg:border-none lg:flex lg:flex-row lg:w-auto`}
+                className={`${
+                    openNav ? "opacity-100 translate-y-10" : "opacity-0"
+                } transition flex flex-col w-full absolute left-0 pb-4 lg:pb-0 bg-white dark:bg-slate-900 border-b-2 lg:opacity-100 lg:relative lg:top-0 lg:border-none lg:flex lg:flex-row lg:w-auto`}
             >
                 {renderHomepageLinks}
             </ul>
