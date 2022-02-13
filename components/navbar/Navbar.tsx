a73b4450b0f639be963582f4fce5c21da58181e3
Blake Bradford <bbradforddesign@gmail.com>
2022-02-12 23:15:32 -0600

    nav menu animation overhaul; switched menu icon for css solution

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 
 type Props = {
@@ -11,11 +10,9 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
 
     // render links to homepage sections; not generated pages
     const renderHomepageLinks = homepageLinks.map((e) => (
-        <li className="m-2 lg:my-0" key={e}>
+        <li className="ml-12 my-2 w-fit lg:my-0 lg:ml-4" key={e}>
             <Link href={`#${e}`}>
-                <a className="font-semibold hover:text-blue-600 dark:hover:text-yellow-300">
-                    {e}
-                </a>
+                <a className="nav-link">{e}</a>
             </Link>
         </li>
     ));
@@ -28,19 +25,22 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
                         <h1 className="font-bold text-xl">bbradforddesign</h1>
                     </a>
                 </Link>
-                <GiHamburgerMenu
-                    className={`text-xl mr-2 ${
-                        openNav
-                            ? "rotate-90 animate-menu-active"
-                            : "animate-menu-inactive"
-                    } lg:hidden`}
+                <button
+                    className="lg:hidden relative h-6 w-8"
+                    aria-label="navigation toggle"
                     onClick={() => setOpenNav((openNav) => !openNav)}
-                />
+                >
+                    <span
+                        className={`menu-button ${
+                            openNav ? "menu-active" : "menu-inactive"
+                        }`}
+                    />
+                </button>
             </div>
             <ul
                 className={`${openNav || "hidden"} ${
                     !openNav || "flex"
-                } flex-col w-full absolute top-14 left-0 bg-white dark:bg-slate-900 border-b-2 lg:relative lg:top-0 lg:border-none lg:flex lg:flex-row lg:w-auto`}
+                } flex-col w-full absolute top-14 left-0 pb-4 lg:pb-0 bg-white dark:bg-slate-900 border-b-2 lg:relative lg:top-0 lg:border-none lg:flex lg:flex-row lg:w-auto`}
             >
                 {renderHomepageLinks}
             </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,33 +8,5 @@ module.exports = {
             safelist: ["dark"],
         },
     },
-    theme: {
-        extend: {
-            keyframes: {
-                "menu-active": {
-                    "0%": {
-                        transform: "rotate(0deg)",
-                    },
-                    "50%": {
-                        transform: "scale(0.5)",
-                        opacity: "0",
-                    },
-                },
-                "menu-inactive": {
-                    "0%": {
-                        transform: "rotate(90deg)",
-                    },
-                    "50%": {
-                        transform: "scale(0.5)",
-                        opacity: "0",
-                    },
-                },
-            },
-            animation: {
-                "menu-active": "menu-active 0.25s ease-out",
-                "menu-inactive": "menu-inactive 0.25s ease",
-            },
-        },
-    },
     plugins: [],
 };
