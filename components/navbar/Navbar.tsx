e36f79148810ce9b831e72f7dbb99233f2f0a9b7
Blake Bradford <bbradforddesign@gmail.com>
2022-02-13 01:27:06 -0600

    adding backslash to links to test mobile flicker

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
     // render links to homepage sections; not generated pages
     const renderHomepageLinks = homepageLinks.map((e) => (
         <li className="ml-12 my-2 w-fit lg:my-0 lg:ml-4" key={e}>
-            <Link href={`#${e}`}>
+            <Link href={`/#${e}`}>
                 <a className="nav-link">{e}</a>
             </Link>
         </li>
@@ -20,7 +20,7 @@ export const Navbar: React.FC<Props> = ({ homepageLinks }) => {
     return (
         <nav className="flex flex-col justify-between items-start p-4 w-full sticky top-0 -mb-12 z-10 opacity-95 backdrop-blur-lg bg-white dark:bg-slate-900 border-b-2 lg:flex-row lg:items-center">
             <div className="flex flex-row justify-between items-center w-full">
-                <Link href="#">
+                <Link href="/#">
                     <a>
                         <h1 className="font-bold text-xl">bbradforddesign</h1>
                     </a>
