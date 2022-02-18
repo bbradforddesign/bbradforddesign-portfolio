010467ba7aa3a25e60b62f1addd16df1abad8b46
Blake Bradford <bbradforddesign@gmail.com>
2022-02-18 16:27:24 -0600

    corrected display style for layout header

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout: React.FC<Props> = ({ children }) => {
     return (
         <ThemeProvider attribute="class">
             <div className="min-h-screen flex flex-col space-between">
-                <header className="sticky z-40 absolute top-0 -mb-12">
+                <header className="sticky z-40 top-0 -mb-12">
                     <Navbar />
                 </header>
                 <main className="bg-slate-50 dark:bg-slate-900 py-12 px-8">
